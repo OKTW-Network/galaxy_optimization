1a2e7f2d6687246f18d0c24cd14b068047d3ddaa
jimchen5209 <jimchen5209@gmail.com>
2026-01-05 22:29:51 +0800

    feat: initial commit

---
+++ common/src/main/java/one/oktw/galaxy_optimization/Constants.java
@@ -0,0 +1,11 @@
+package one.oktw.galaxy_optimization;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "galaxy_optimization";
+    public static final String MOD_NAME = "galaxy_optimization";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ neoforge/src/main/java/one/oktw/galaxy_optimization/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,23 @@
+package one.oktw.galaxy_optimization.platform;
+
+import one.oktw.galaxy_optimization.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+        return !FMLLoader.isProduction();
+    }
+}
